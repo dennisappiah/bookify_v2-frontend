6134b054c771c5c9aece6f1ac64c81d0379a4a08
dennisappiahALX <appiahdennis1998@gmail.com>
2023-03-14 16:30:16 +0000

    main app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import BooksForm from './pages/BooksForm'
+import NotFound from './pages/NotFound'
+import RegisterForm from './pages/RegisterForm'
+import LoginForm from './pages/LoginForm'
+import Logout from './pages/Logout'
+//import Components
+import NavBar from './components/NavBar'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overflow-hidden  container">
+        {/* Register Routes and Components */}
+        <Router>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/books/:id' element={<BooksForm />}/>
+            <Route path='/books' element={<Home />}/>
+            <Route path='/register' element={<RegisterForm />}/>
+            <Route path='/login' element={<LoginForm />}/>
+            <Route path='/logout' element={<Logout />}/>
+            <Route path="*" element={<NotFound/>} />
+          </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
