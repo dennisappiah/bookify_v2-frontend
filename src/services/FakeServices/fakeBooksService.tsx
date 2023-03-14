799ba361d51c56535f7c5280d0b8e60404387d64
dennisappiahALX <appiahdennis1998@gmail.com>
2023-03-14 16:25:45 +0000

    fake services

---
+++ src/services/FakeServices/fakeBooksService.tsx
@@ -0,0 +1,95 @@
+import * as categoryAPI from "./faksCategoryServices";
+import {Book} from "../../models/Book";
+
+
+const books = [
+    {
+        _id: "5b21ca3eeb767fhccd471815",
+        title : "Terminator",
+        category : {_id:"5b21ca3eeb767fhccd471818", name: "Action" },
+        numberInStock: 5,
+        dailyRentalRate : 2.5,
+        liked: true
+    }, 
+    {
+        _id: "5b21ca3eeb767fhccd471816",
+        title : "Avatar",
+        category : {_id:"5b21ca3eeb767fhccd471818", name: "Action" },
+        numberInStock: 6,
+        dailyRentalRate : 10
+    },
+    {
+        _id: "5b21ca3eeb767fhccd471817",
+        title : "Woman Steel",
+        category : {_id:"5b21ca3eeb767fhccd471819", name: "Romance" },
+        numberInStock: 7,
+        dailyRentalRate : 15
+    },
+    {
+        _id: "5b21ca3eeb767fhccd471818",
+        title : "Selfish Love",
+        category : {_id:"5b21ca3eeb767fhccd471819", name: "Romance" },
+        numberInStock: 8,
+        dailyRentalRate : 16
+    },
+    {
+        _id: "5b21ca3eeb767fhccd471819",
+        title : "Televista",
+        category : {_id:"5b21ca3eeb767fhccd471820", name: "Adventure" },
+        numberInStock: 9,
+        dailyRentalRate : 17
+    },
+    {
+        _id: "5b21ca3eeb767fhccd471820",
+        title : "Hello World",
+        category : {_id:"5b21ca3eeb767fhccd471820", name: "Adventure" },
+        numberInStock: 15,
+        dailyRentalRate : 20
+    },
+    {
+        _id: "5b21ca3eeb767fhccd471821",
+        title : "World Season",
+        category : {_id:"5b21ca3eeb767fhccd471821", name: "Thriller" },
+        numberInStock: 15,
+        dailyRentalRate : 20
+    },
+    {
+        _id: "5b21ca3eeb767fhccd471822",
+        title : "Amazon",
+        category : {_id:"5b21ca3eeb767fhccd471822", name: "Comedy" },
+        numberInStock: 15,
+        dailyRentalRate : 20
+    },
+    {
+        _id: "5b21ca3eeb767fhccd471823",
+        title : "Amazon Crashers",
+        category : {_id:"5b21ca3eeb767fhccd471822", name: "Comedy" },
+        numberInStock: 15,
+        dailyRentalRate : 20
+    },
+
+]
+
+export const getbooks = () => {
+    return books;
+}
+
+export const getBookById = (_id:string) => {
+    return books.find(book => book._id === _id);
+}
+
+// export function saveBook(book: Book) {
+//     let bookInDb = books.find(m => m._id === book._id);
+//     bookInDb.title = book.title;
+//     bookInDb.category = categoryAPI.genres.find(g => g._id === book.genreId);
+//     bookInDb.numberInStock = book.numberInStock;
+//     bookInDb.dailyRentalRate = book.dailyRentalRate;
+  
+//     if (!bookInDb._id) {
+//       movieInDb._id = Date.now().toString()
+//       books.push(movieInDb);
+//     }
+  
+//     return movieInDb;
+//   }
+  
---
+++ src/services/FakeServices/faksCategoryServices.tsx
@@ -0,0 +1,14 @@
+export const category = [
+    { _id: "5b21ca3eeb767fhccd471818", name: "Action" },
+  
+    { _id: "5b21ca3eeb767fhccd471819", name: "Romance" },
+    { _id: "5b21ca3eeb767fhccd471820", name: "Adventure" },
+    { _id: "5b21ca3eeb767fhccd471821", name: "Thriller" },
+    { _id: "5b21ca3eeb767fhccd471822", name: "Comedy" }
+  ];
+  
+  export function getCategory() {
+    return category.filter(g => {
+      return g.name
+    });
+  }
