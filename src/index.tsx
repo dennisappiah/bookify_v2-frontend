eabd42f9a5628bbc349cd5d37987ff3e218d2fdf
dennisappiahALX <appiahdennis1998@gmail.com>
2023-03-16 22:47:17 +0000

    main app

--- src/index.tsx
+++ src/index.tsx
@@ -8,18 +8,26 @@ import 'font-awesome/css/font-awesome.css'
 import BookProvider from './context/BookContext';
 // import categoriesProvider
 import CategoryProvider from './context/CategoryContext';
+//import AuthProvider
+import AuthProvider from './context/AuthContext';
+//import RegisterProvider
+import RegisterProvider from './context/UserContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <CategoryProvider>
-    <BookProvider>
-      <React.StrictMode>
-      <App />
-    </React.StrictMode>
-    </BookProvider>
-  </CategoryProvider>
+  <RegisterProvider>
+    <AuthProvider >
+    <CategoryProvider>
+      <BookProvider>
+        <React.StrictMode>
+        <App />
+      </React.StrictMode>
+      </BookProvider>
+    </CategoryProvider>
+  </AuthProvider>
+  </RegisterProvider>
   
 );
 
