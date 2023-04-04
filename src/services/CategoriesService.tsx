1a46b03dea2ad9f34c1902e5791b6438cc8eba34
dennisappiahALX <appiahdennis1998@gmail.com>
2023-04-04 08:59:19 +0000

    dele

--- src/services/CategoriesService.tsx
+++
@@ -1,8 +0,0 @@
-import {Category} from '../models/Category';
-import axiosInstance from './axiosInstance';
-
-
-export const getCategory = async() => {
-    const response = await axiosInstance.get<Category[]>('/categories');
-    return response.data
-  };
