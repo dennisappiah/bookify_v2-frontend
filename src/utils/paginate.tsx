c11282cf6fda7972f505f82f1c564a4755f1559d
dennisappiahALX <appiahdennis1998@gmail.com>
2023-04-04 10:14:23 +0000

    utils

---
+++ src/utils/paginate.tsx
@@ -0,0 +1,13 @@
+import _ from "lodash-es";
+
+// function that paginate array of data
+export function Paginate<T>(
+  arrayItems: T[],
+  pageNumber: number,
+  pageSize: number
+) {
+  const startIndex = (pageNumber - 1) * pageSize;
+
+  // returns a lodash object
+  return _.chain(arrayItems).slice(startIndex).take(pageSize).value();
+}
