f2eafb8b062ec97102ac8dabeb3c01493ef52fec
dennisappiahALX <appiahdennis1998@gmail.com>
2023-04-04 10:13:56 +0000

    schemas

---
+++ src/schemas/booksSchema.tsx
@@ -0,0 +1,19 @@
+import * as yup from "yup";
+
+// const categoryIdPattern = /^[0-9a-fA-F]{24}$/;
+
+export const BooksFormSchema = yup.object().shape({
+  title: yup.string().required("Title is required"),
+  categoryId: yup.string().required("Category is required"),
+  // categoryId: yup.string().matches(categoryIdPattern, 'Invalid category ID').required('Category is required'),
+  numberInStock: yup
+    .number()
+    .required("Number in stock is required")
+    .positive("Number in stock must be a positive number"),
+  dailyRentalRate: yup
+    .number()
+    .required("Daily rental rate is required")
+    .positive("Daily rental rate must be a positive number"),
+});
+
+export default BooksFormSchema;
---
+++ src/schemas/loginSchema.tsx
@@ -0,0 +1,14 @@
+import * as yup from "yup";
+
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, "Username must be at least 3 characters long")
+    .required("Username is Required"),
+  password: yup
+    .string()
+    .min(5, "Password must be at least 5 characters long")
+    .required("Password is Required"),
+});
+
+export default loginSchema;
